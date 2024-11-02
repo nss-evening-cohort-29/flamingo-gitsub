27472add371f848e27fd4d19c400b3291c47d1be
AndrewDCLA <andrewdavila.work@gmail.com>
2024-11-01 19:06:50 -0500

    Added data.js to centralize all data array obects and included export and import functions to use the arrays

---
+++ data.js
@@ -0,0 +1,256 @@
+// Need an array object for Repositories
+
+const reposPreMade = [
+  { 
+    name: "test-repo000",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isStarred: true,
+   },
+
+   { 
+    name: "test-repo001",
+    primaryLanguage: "html",
+    publicOrPrivate: "public",
+    isStarred: true,
+   },
+
+   { 
+    name: "test-repo002",
+    primaryLanguage: "python",
+    publicOrPrivate: "public",
+    isStarred: true,
+   },
+
+   { 
+    name: "test-repo003",
+    primaryLanguage: "rust",
+    publicOrPrivate: "public",
+    isStarred: true,
+   },
+
+   { 
+    name: "test-repo004",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isStarred: true,
+   },
+
+   { 
+    name: "test-repo005",
+    primaryLanguage: "c++",
+    publicOrPrivate: "public",
+    isStarred: true,
+   },
+
+   { 
+    name: "test-repo006",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isStarred: false,
+   },
+
+   { 
+    name: "test-repo007",
+    primaryLanguage: "html",
+    publicOrPrivate: "private",
+    isStarred: false,
+   },
+
+   { 
+    name: "test-repo008",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isStarred: false,
+   },
+
+   { 
+    name: "test-repo009",
+    primaryLanguage: "c#",
+    publicOrPrivate: "private",
+    isStarred: false,
+   },
+
+   { 
+    name: "test-repo010",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isStarred: false,
+   },
+
+]
+
+
+
+// Need an array object for Projects
+
+const projectsPreMade = [
+  { 
+    name: "test-project000",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "test-project001",
+    primaryLanguage: "html",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "test-project002",
+    primaryLanguage: "python",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "cat-tracker-app",
+    primaryLanguage: "rust",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "cookie-recipe-generator",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "get-the-weather",
+    primaryLanguage: "c++",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "news-scraper",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isOpen: false,
+   },
+
+   { 
+    name: "word-press-killer",
+    primaryLanguage: "html",
+    publicOrPrivate: "private",
+    isOpen: false,
+   },
+
+   { 
+    name: "choose-your-own-adventure-game",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isOpen: false,
+   },
+
+   { 
+    name: "book-organizer",
+    primaryLanguage: "c#",
+    publicOrPrivate: "private",
+    isOpen: false,
+   },
+
+   { 
+    name: "todo-list",
+    primaryLanguage: "javascript",
+    publicOrPrivate: "public",
+    isOpen: false,
+   },
+
+]
+
+
+
+
+
+// Need an array object for Packages
+
+
+const packagesPreMade = [
+  { 
+    name: "test-package000",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "test-package001",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "test-package002",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "Apple Kernel Corer",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "Banana Peel Scraper",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "Grape Bundler",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: true,
+   },
+
+   { 
+    name: "Orange Code Juicer",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: false,
+   },
+
+   { 
+    name: "Chocolate Bar Segmenter",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "private",
+    isOpen: false,
+   },
+
+   { 
+    name: "Peppermint Log Logger",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: false,
+   },
+
+   { 
+    name: "Caramel Carrier",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "private",
+    isOpen: false,
+   },
+
+   { 
+    name: "Mystery Meat",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis!",
+    publicOrPrivate: "public",
+    isOpen: false,
+   },
+
+]
+
+// Complex data object 
+
+export {reposPreMade, projectsPreMade, packagesPreMade};
--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 console.log("yay");
+
+// importing from the data module
+import {reposPreMade, projectsPreMade, packagesPreMade} from'./data.js'
