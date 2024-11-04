3c0fc08ecbda0283305bebc131f7bba1b9eac8c6
Alyssa Cleland <alyssamariecleland@gmail.com>
2024-11-03 23:34:08 -0600

    added pinning functionality within repos page

--- main.js
+++ main.js
@@ -33,7 +33,9 @@ const overviewCardsOnDom = (array) => {
     </div>
   </div>`;
   }
-  overviewDynamicContainer.innerHTML = domString;
+  if (document.URL.includes("index.html")) {
+    overviewDynamicContainer.innerHTML = domString;
+  }
 };
 
 // call the function with the pinnedRepos array to paint the DOM
@@ -102,6 +104,30 @@ if (document.URL.includes("index.html")) {
 //************************************//
 
 //************************************//
+//*********PINNED CARDS ON REPOS DOM**//
+//************************************//
+
+const pinnedReposCardsOnDom = (array) => {
+  let pinnedDomString = "";
+  for (const pinnedRepo of array) {
+    pinnedDomString +=
+      //has pin button like the repo cards, but it's not clickable on these
+      `<div class="card" id="dynamic-cards">
+    <div class="card-body">
+    <p><a class="link-offset-2 link-underline link-underline-opacity-0" href="#" id="pin--">⭐Pinned</a></p>
+      <h5 class="card-title">${pinnedRepo.name}</h5>
+      <p class="card-text">${pinnedRepo.description}</p>
+      <a href="#" class="card-link">${pinnedRepo.primaryLanguage}</a>
+      <a href="#" class="card-link">${pinnedRepo.publicOrPrivate}</a>
+    </div>
+  </div>`;
+  }
+  if (document.URL.includes("repos.html")) {
+    document.querySelector("#pinnedFromReposPage").innerHTML += pinnedDomString;
+  }
+};
+
+//************************************//
 //**********CARDS ON DOM**************//
 //************************************//
 
@@ -117,7 +143,7 @@ const reposCardsOnDom = (array) => {
       //"pin-button--" the -- is to add pinning functionality later, in a similar fashion to delete
       `<div class="card" id="dynamic-cards">
   <div class="card-body">
-  <p><a class="link-offset-2 link-underline link-underline-opacity-0" href="#" id="pin--">⭐Pin (adding functionality later)</a></p>
+  <p><a class="link-offset-2 link-underline link-underline-opacity-0" href="#" id="pin--${repo.id}">Pin</a></p>
     <h5 class="card-title">${repo.name}</h5>
     <p class="card-text">${repo.description}</p>
     <a href="#" class="card-link">${repo.primaryLanguage}</a>
@@ -184,6 +210,40 @@ if (document.URL.includes("repos.html")) {
     reposCardsOnDom(repos);
     //clear the form after submit
     form.reset();
+  });
+}
+
+//************************************//
+//**********PINNING*******************//
+//************************************//
+
+//target the right div. above we assigned reposDynamicContainer to target our div with the id "#repos-dynamicContainer"
+if (document.URL.includes("repos.html")) {
+  reposDynamicContainer.addEventListener("click", (e) => {
+    //check that e.target.id includes "pin", includes() method returns true if an array contains a specific value
+    //target finds that specific delete button (since there are multiple on the page)
+    if (e.target.id.includes("pin")) {
+      //destructuring
+      //const [, id]: the comma is a placeholder, indicating that the first value of the array is ignored. The second value (whatever is after the "--") is assigned to the const "id," which we are also delcaring here
+      //above, in the cards, the pin button's ID is: id="pin--${repos.id}". with e.taget.id, we are retrieving that, and we are splitting it at the --, which gives us the array [expel, #].
+      //we assign const id = #
+      //we're basically just getting the # id of the card who's expel button we clicked, and assigning that to const id
+      const [, id] = e.target.id.split("--");
+      //The findIndex() method of Array instances returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned.
+      //so here, we are finding the first instance that a repos id matches the const id that we assigned in the previous step. And we are assigning that to the const "index"
+      //// Number() just makes sure id is converted to a number (if it's not already)
+      //tldr: we're finding where the id of an obj in repos matches the id of where we clicked, and assigning that to const index
+      const index = repos.findIndex((e) => e.id === Number(id));
+      //remove index (which we just declared and assigned in the previous step) from the array, and assign it to const removed
+      ////splice can do other things, but in this case (X, #) or for us (index, 1), X is the position of the item to remove, and # is the number of elements to remove, starting from X.
+      //also we assign the thing that we removed to a const "removed", so that we can pass it into overviewCardsOnDom
+      const removed = repos.splice(index, 1);
+      reposCardsOnDom(repos);
+      if (document.URL.includes("repos.html")) {
+        // overviewCardsOnDom(pinnedRepos); not relevant since reloading (or clicking the overview page) doesn't retain data pushes. doing it within the same page now, to at least have something.
+        pinnedReposCardsOnDom(removed);
+      }
+    }
   });
 }
 
@@ -367,4 +427,4 @@ if (document.URL.includes("packages.html")) {
 }
 
 //this logs the entire HTML after all dynamic rendering is complete. useful for debugging or seeing what our resulting html is. can delete later.
-console.log(document.documentElement.outerHTML);
+// console.log(document.documentElement.outerHTML);
